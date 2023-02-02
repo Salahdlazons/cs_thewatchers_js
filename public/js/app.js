40dfb915c975f561262ba6b4cf1e6279593b6548
salahdlazons <salahdin.azzouzi0@gmail.com>
2023-02-02 15:07:31 +0100

    section choice fini

--- public/js/app.js
+++ public/js/app.js
@@ -12,51 +12,29 @@
     sJS.append(section2);
     section2.id = 'section2-container';
 
-    let price = document.createElement('p');
-    price.textContent = '$ 45,743'
+    
 
-    let productName = document.createElement('h3');
-    productName.textContent = 'Thermo Ball Etip Gloves'
-
-    let img1 = document.createElement('div');
 
     let TroisDiv3 = document.createElement('div');
-    sJS.append(img1)
-    img1.append(TroisDiv3);
+    sJS.append(TroisDiv3)
     TroisDiv3.classList.add('tresmuntros');
     TroisDiv3.classList.add('watchJs1');
-    
-    img1.append(productName)
-    img1.append(price)
+    TroisDiv3.innerHTML = '<p class="M-T" class="fontJs">Thermo Ball Etip Gloves</p>'
     
 
 
-    let img2 = document.createElement('div');
+    
     
     let TroisDiv2 = document.createElement('div');
     sJS.append(TroisDiv2);
-    img2.prepend(TroisDiv2);
     TroisDiv2.classList.add('tresmuntros');
     TroisDiv2.classList.add('watchJs2');
 
 
-    img2.prepend(productName)
-    img2.prepend(price)
-
-
-    let img3 = document.createElement('div');
-    
     let TroisDiv1 = document.createElement('div');
-
     sJS.append(TroisDiv1);
-    img3.prepend(TroisDiv1);
-
     TroisDiv1.classList.add('tresmuntros');
     TroisDiv1.classList.add('watchJs3');
-
-    
-    img3.prepend(productName)
-    img3.prepend(price)
     
     section2.append(TroisDiv3)
     section2.append(TroisDiv2)
